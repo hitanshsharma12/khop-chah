7fab46f80b7243d24c9a56cabfd3474c3c25df3c
hitanshsharma12 <hitanshsharma6798@gmail.com>
2026-04-08 10:56:14 +0530

    build error fixed

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -6,16 +6,23 @@ import MenuList from "../components/MenuList";
 
 export default function MenuPage() {
   const [category, setCategory] = useState("All Items");
+  const [cart, setCart] = useState<any[]>([]);
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="min-h-screen bg-[#c06600] p-6">
 
-      <h1 className="text-3xl font-bold mb-6 text-center">
+      <h1 className="text-3xl font-bold mb-6 text-center text-white">
         Our Menu
       </h1>
 
       <MenuTabs setCategory={setCategory} />
-      <MenuList category={category} />
+
+      <MenuList
+        category={category}
+        setCart={setCart}
+        setOpen={setOpen}
+      />
 
     </div>
   );
